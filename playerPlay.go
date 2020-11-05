b14c1b414bb136790a65efe3c35a754a70b94291
iComputeDaily <henry.haines@thetwelfth.com>
2020-11-05 10:50:31 -0500

    Do rudementary nbt encoding for the packet

--- playerPlay.go
+++ playerPlay.go
@@ -5,6 +5,13 @@ import "github.com/Tnze/go-mc/net/packet"
 import "fmt"
 import "bytes"
 import "github.com/Tnze/go-mc/nbt"
+// import "errors"
+
+type nbtWrap struct {
+	Value interface{} // Holds struct of nbt data
+	buf *bytes.Buffer // Holds encoded nbt
+	enc *nbt.Encoder  // An encoder object
+}
 
 type dimensionCodec struct {
 	DimensionType struct {
@@ -43,29 +50,51 @@ type biome struct {
 	
 }
 
+func (nbtData nbtWrap) Encode() []byte {
+	if nbtData.buf == nil {
+		// make an empty buffer
+		nbtData.buf = &bytes.Buffer{}
+	}
+	
+	if nbtData.enc == nil {
+		// make an encoder on the buffer
+		nbtData.enc = nbt.NewEncoder(nbtData.buf)
+	}
+	
+	// clear the buffer - allows memory to be reused
+	nbtData.buf.Reset()
+	
+	// encode to the buffer
+	err := nbtData.enc.Encode(nbtData.Value)
+	
+	if err != nil {
+		panic(fmt.Sprintln("Failed To Encode! Error: ", err))
+	}
+	
+	// get bytes from the buffer
+	return nbtData.buf.Bytes()
+}
+
 func (player *Player) handlePlaying() {
 	player.sendJoinGame()
 }
 
 func (player *Player) sendJoinGame() {
+	var nbtDimCodec nbtWrap
 	var dimCodec dimensionCodec
-	var buf bytes.Buffer
+	nbtDimCodec.Value = dimCodec
 	
-	err := nbt.Marshal(&buf, dimCodec);
-	if err != nil {
-		fmt.Println("Could Not Marshal NBT Data! Error: ", err)
-		return
-	}
+	fmt.Printf("\n%+v\n\n", nbtDimCodec)
 	
-	err = player.connection.WritePacket(packet.Marshal(0x24,
+	err := player.connection.WritePacket(packet.Marshal(0x24,
 		packet.Int(0),                        // entity id
 		packet.Boolean(false),                // is hardcore
 		packet.UnsignedByte(3),               // gamemode
 		packet.UnsignedByte(3),               // "previous gamemode"
 		packet.VarInt(0),                     // world count
 		packet.String("minecraft:coolworld"), // world names(array)
-		packet.NBT(buf),                         // dimension codec
-//		packet.NBT(),                         // dimention
+		nbtDimCodec,                          // dimension codec
+//		nbtDim,                               // dimention
 		packet.String("minecraft:coolworld"), // world name
 		packet.Long(0),                       // hashed seed
 		packet.VarInt(1),                     // max players
